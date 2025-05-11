34a8b17c8faccee5a8bd44a5a745fb1b96563273
findjob-click <coder@findjob.click>
2025-05-11 03:27:59 +0000

    Add S3 static website deployment for index.html

---
+++ s3.tf
@@ -0,0 +1,35 @@
+resource "aws_s3_bucket" "website" {
+  bucket = "findjob.click"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "index.html"
+  source       = "${path.module}/index.html"
+  content_type = "text/html"
+  acl          = "public-read"
+}
