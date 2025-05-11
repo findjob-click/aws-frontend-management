f95864d9b33ff684aaef4ccd43cb93b7a36e6650
findjob-click <coder@findjob.click>
2025-05-11 03:43:30 +0000

    add s3 object

--- s3.tf
+++ s3.tf
@@ -22,3 +22,10 @@ resource "aws_s3_bucket_public_access_block" "website" {
   restrict_public_buckets = false
   ignore_public_acls      = false
 }
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "index.html"
+  source       = "${path.module}/index.html"
+  content_type = "text/html"
+}
