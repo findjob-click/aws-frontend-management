f58cf337018e241e1f0f9650d9cbae19a6f53ee9
findjob-click <coder@findjob.click>
2025-05-11 21:02:36 +0000

    removed trailing slash

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_lambda_function" "linkedin_login" {
   variables = {
     LINKEDIN_CLIENT_ID     = var.linkedin_client_id
     LINKEDIN_CLIENT_SECRET = var.linkedin_client_secret
-    REDIRECT_URI           = "https://ki1y9x9avk.execute-api.us-east-1.amazonaws.com/"
+    REDIRECT_URI           = "https://ki1y9x9avk.execute-api.us-east-1.amazonaws.com"
   }
 }
 }
