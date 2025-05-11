09ffb8d79c42c05481c6d4cf1a9872d851babe76
findjob-click <coder@findjob.click>
2025-05-11 03:57:30 +0000

    cloudfront distribution

---
+++ cloudfront.tf
@@ -0,0 +1,111 @@
+resource "aws_acm_certificate" "findjob_ssl" {
+  domain_name       = "findjob.click"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.findjob_ssl.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.findjob_ssl.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+resource "aws_cloudfront_distribution" "website" {
+  origin {
+    domain_name = "${aws_s3_bucket.website.bucket}.s3-website-us-east-1.amazonaws.com"
+    origin_id   = "s3-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+    depends_on = [
+    aws_s3_bucket_website_configuration.website_config,
+    aws_acm_certificate_validation.cert
+  ]
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CDN for findjob.click"
+  default_root_object = "index.html"
+
+  aliases = ["findjob.click"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.cert.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "FindJobClick CloudFront"
+  }
+}
+
+data "aws_route53_zone" "primary" {
+  name         = "findjob.click"
+  private_zone = false
+}
+
+resource "aws_route53_record" "findjob_click_alias" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "findjob.click"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
+  depends_on = [
+    aws_cloudfront_distribution.website
+  ]
+}
